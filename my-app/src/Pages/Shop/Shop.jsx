f3582f92b4e18fec16519a1d49e1a9c6c688d870
hassanali293 <mhassanali600@gmail.com>
2023-07-24 12:38:24 +0500

    Shop page update

--- my-app/src/Pages/Shop/Shop.jsx
+++ my-app/src/Pages/Shop/Shop.jsx
@@ -59,7 +59,6 @@ const Shop = () => {
         </p>
       </div>
 
-      <div className="container">
         <div className="row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-3 g-2 g-lg-3">
           {products.map((product, index) => (
             <div className="col" key={index}>
